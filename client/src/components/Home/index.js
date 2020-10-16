a9e88a89baad1486d7b5aad0071a4d23e35c0b11
zx012 <rrai9999999@gmail.com>
2020-10-17 01:17:29 +0530

    Basic frontend added

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+export default function () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simple Chat Room App</h1>
+        <div className="buttons">
+          <Button
+            color="secondary"
+            variant="contained"
+            component={Link}
+            to="/login"
+          >
+            Login
+          </Button>
+          <Button
+            color="primary"
+            variant="contained"
+            component={Link}
+            to="/register"
+          >
+            Register
+          </Button>
+        </div>
+      </header>
+    </div>
+  );
+}
---
+++ client/src/components/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import Chat from "./Chat";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/register" component={Register} exact />
+        <Route path="/chat" component={Chat} exact />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
