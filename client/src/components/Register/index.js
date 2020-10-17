dffbad5bcfb3fe606a27321f12146c0267fb140e
zx012 <rrai9999999@gmail.com>
2020-10-18 00:53:53 +0530

    Starting to add MongoDB

---
+++ client/src/components/Register/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import "./style.css";
+//import {apiCall} from "../../utility";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registerUser() {
+    const res = fetch("http://localhost:1337/api/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }).then((t) => t.json());
+
+    console.log(res);
+  }
+
+  return (
+    <div className="form">
+      <h1>Register</h1>
+      <form className="register-fields">
+        <TextField
+          label="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          variant="outlined"
+        />
+        <TextField
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          variant="outlined"
+        />
+        <Button color="primary" variant="contained" onClick={registerUser}>
+          Register
+        </Button>
+      </form>
+    </div>
+  );
+}
--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/api/register", (req, res) => {
+  console.log(req.body);
+  res.json({ status: "ok" });
 });
 
 app.listen(1337);
